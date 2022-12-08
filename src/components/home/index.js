17f271ebc7737781ccb8397799c8c069b7dbf533
Shashwat <shashwatrathod22@gmail.com>
2022-12-08 05:08:48 -0500

    added create-tuit component

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,68 +2,30 @@ import React, { useContext } from "react";
 import Tuits from "../tuits";
 import * as service from "../../services/tuits-service";
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
 import { UserContext } from "../../contexts/user-context";
+import CreateTuit from "../tuits/create-tuit";
 
 const Home = () => {
-  const location = useLocation();
   const { user } = useContext(UserContext);
   const [tuits, setTuits] = useState([]);
-  const [tuit, setTuit] = useState("");
 
   const findTuits = () => {
     return service.findAllTuits().then((tuits) => setTuits(tuits));
   };
   useEffect(() => {
-    let isMounted = true;
     findTuits();
-    return () => {
-      isMounted = false;
-    };
   }, []);
-  const createTuit = () =>
-    user && service.createTuit(user._id, { tuit }).then(findTuits);
+
+  const createTuit = (tuit) =>
+    user && service.createTuit(user._id, tuit).then(findTuits);
+
   const deleteTuit = (tid) => user && service.deleteTuit(tid).then(findTuits);
+
   return (
     <div className="ttr-home">
       <div className="border border-bottom-0">
         <h4 className="fw-bold p-2">Home Screen</h4>
-        {user && (
-          <div className="d-flex">
-            <div className="p-2">
-              <img
-                className="ttr-width-50px rounded-circle"
-                src="../images/nasa-logo.jpg"
-              />
-            </div>
-            <div className="p-2 w-100">
-              <textarea
-                onChange={(e) => setTuit(e.target.value)}
-                placeholder="What's happening?"
-                className="w-100 border-0"
-              ></textarea>
-              <div className="row">
-                <div className="col-10 ttr-font-size-150pc text-primary">
-                  <i className="fas fa-portrait me-3"></i>
-                  <i className="far fa-gif me-3"></i>
-                  <i className="far fa-bar-chart me-3"></i>
-                  <i className="far fa-face-smile me-3"></i>
-                  <i className="far fa-calendar me-3"></i>
-                  <i className="far fa-map-location me-3"></i>
-                </div>
-                <div className="col-2">
-                  <a
-                    onClick={createTuit}
-                    className={`btn btn-primary rounded-pill fa-pull-right
-                                  fw-bold ps-4 pe-4`}
-                  >
-                    Tuit
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+        {user && <CreateTuit tuitOnClick={createTuit} />}
       </div>
       <Tuits tuits={tuits} deleteTuit={deleteTuit} refreshTuits={findTuits} />
     </div>
