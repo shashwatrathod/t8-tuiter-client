52421cb55249857de7000f254d93c87a31b09ce7
Aisha Kothare <aishakothare31@gmail.com>
2022-12-12 12:58:55 -0500

    minor change to base urls: include url specified in env var

--- src/services/usermentions-service.js
+++ src/services/usermentions-service.js
@@ -1,9 +1,6 @@
 import axios from "axios";
-//const BASE_URL =
-//  process.env.REACT_APP_TUIT_SERVICE_URL ||
-//  "https://shash-tuiter.herokuapp.com/api";
-// const BASE_URL = "https://software-engineering-node-fa22.herokuapp.com/api";
- const BASE_URL = "http://localhost:4000/api" || "http://localhost:4003/api";
+const BASE_URL =
+ process.env.REACT_APP_TUIT_SERVICE_URL || "http://localhost:4000/api";
 
 
 
